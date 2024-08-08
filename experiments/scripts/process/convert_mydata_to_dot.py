e40ee76ca2b489890c333711f90ca681365cbd5a
AlonEirew <alon.eirew@gmail.com>
2024-08-08 10:23:43 +0300

    committing mydata

--- experiments/scripts/process/convert_mydata_to_dot.py
+++ experiments/scripts/process/convert_mydata_to_dot.py
@@ -1,5 +1,6 @@
 import json
 import os
+from collections import OrderedDict
 
 
 def read_mentions(mentions):
@@ -10,16 +11,21 @@ def read_mentions(mentions):
 
 
 def read_files_from_folder(folder_path):
-    files_content = {}
+    files_content = OrderedDict()
+    files_list = list()
     try:
         for filename in os.listdir(folder_path):
             file_path = os.path.join(folder_path, filename)
             if os.path.isfile(file_path):
-                with open(file_path, 'r') as file:
-                    data = json.load(file)
-                    mentions = read_mentions(data['allMentions'])
-                    pairs = data['allPairs']
-                    files_content[filename] = {"tokens": data['tokens'], "mentions": mentions, "pairs": pairs}
+                files_list.append(file_path)
+
+        files_list.sort(key=lambda name: int(os.path.basename(name).split("_")[0]))
+        for file_path in files_list:
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                mentions = read_mentions(data['allMentions'])
+                pairs = data['allPairs']
+                files_content[os.path.basename(file.name)] = {"tokens": data['tokens'], "mentions": mentions, "pairs": pairs}
     except Exception as e:
         print(f"An error occurred: {e}")
     return files_content
@@ -45,7 +51,7 @@ if __name__ == '__main__':
     _out_json = dict()
     for _filename, _content in _files_content.items():
         _out_json[_filename] = convert_doc_to_dot(_content)
-        print(f"Filename: {_filename}\nContent:\n{_content}\n")
+        # print(f"Filename: {_filename}\nContent:\n{_content}\n")
 
     with open("data/tmp/my_data_dot.json", 'w', encoding='utf-8') as json_file:
         json.dump(_out_json, json_file, ensure_ascii=False, indent=4)
